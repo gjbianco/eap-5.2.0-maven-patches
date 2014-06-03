fe0376ac207c83626703e1d7a207eb27291f9196
Adam Baxter <abaxter@redhat.com>
2014-06-03 13:45:49 -0400

    Final corrections of script.

--- install_deployables.sh
+++ install_deployables.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 SRC_DIR=./installs
 BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
-UPDATE=BZ-986451.zip
-UPDATE_DIR=./BZ986451
+UPDATE=BZ-1022758.zip
+UPDATE_DIR=./BZ1022758
 VERSION=5.3.1.BRMS
 VERSION_P02=5.3.1.BRMS-P02
 VERSION_P04=5.3.1.BRMS-P04
+VERSION_P05=5.3.1.BRMS-P05
 
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
 
@@ -17,6 +18,11 @@ installPom() {
 installBinary() {
     unzip -q $2-$3.jar META-INF/maven/$1/$2/pom.xml;
     mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$2-$3.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=jar;
+}
+
+installUpdatedBinary() {
+    unzip -q $UPDATE_DIR/$2-$3.jar META-INF/maven/$1/$2/pom.xml;
+    mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$UPDATE_DIR/$2-$3.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=jar;
 }
 
 echo
@@ -54,7 +60,6 @@ installPom org.jbpm  $VERSION_P02
 
 echo Installing P04 parent POMs...
 echo
-installPom org.drools droolsjbpm-parent $VERSION_P04
 installPom org.drools droolsjbpm-knowledge $VERSION_P04
 installPom org.drools drools-multiproject $VERSION_P04
 installPom org.drools droolsjbpm-tools $VERSION_P04
@@ -62,43 +67,54 @@ installPom org.drools droolsjbpm-integration $VERSION_P04
 installPom org.drools guvnor $VERSION_P04
 installPom org.jbpm jbpm $VERSION_P04
 
+echo Installing P05 parent POMs...
+echo
+
+installPom org.drools drools-container $VERSION_P05
+installPom org.drools droolsjbpm-integration $VERSION_P05
+installPom org.drools droolsjbpm-parent $VERSION_P05
+installPom org.drools drools-multiproject $VERSION_P05
+installPom org.drools guvnor $VERSION_P05
+installPom org.jbpm jbpm $VERSION_P05
+
+
 echo Installing Drools binaries...
 echo
 # droolsjbpm-knowledge
-installBinary org.drools knowledge-api
+installUpdatedBinary org.drools knowledge-api $VERSION_02
 # drools-multiproject
-installBinary org.drools drools-core $VERSION_P04
-installBinary org.drools drools-compiler $VERSION_P04
-installBinary org.drools drools-jsr94  $VERSION_P02
-installBinary org.drools drools-verifier $VERSION_P02
-installBinary org.drools drools-persistence-jpa $VERSION_P04
-installBinary org.drools drools-templates $VERSION_P02
-installBinary org.drools drools-decisiontables $VERSION_P02
+installUpdatedBinary org.drools drools-core $VERSION_P05
+installUpdatedBinary org.drools drools-compiler $VERSION_P05
+installUpdatedBinary org.drools drools-jsr94  $VERSION_P02
+installUpdatedBinary org.drools drools-verifier $VERSION_P02
+installUpdatedBinary org.drools drools-persistence-jpa $VERSION_P05
+installUpdatedBinary org.drools drools-templates $VERSION_P02
+installUpdatedBinary org.drools drools-decisiontables $VERSION_P02
 # droolsjbpm-tools
-installBinary org.drools drools-ant
+installBinary org.drools drools-ant $VERSION
 # droolsjbpm-integration
-installBinary org.drools drools-camel
-installBinary org.drools drools-spring
+installBinary org.drools drools-camel $VERSION
+installUpdatedBinary org.drools drools-spring $VERSION_P05
 # guvnor
-installBinary org.drools droolsjbpm-ide-common $VERSION_P02
+installUpdatedBinary org.drools droolsjbpm-ide-common $VERSION_P05
 
 echo Installing jBPM binaries...
 echo
-installBinary org.jbpm jbpm-flow $VERSION_P04
-installBinary org.jbpm jbpm-flow-builder $VERSION_P04
-installBinary org.jbpm jbpm-persistence-jpa $VERSION_P02
-installBinary org.jbpm jbpm-bam $VERSION_P02
-installBinary org.jbpm jbpm-bpmn2 $VERSION_P04
-installBinary org.jbpm jbpm-workitems $VERSION_P02
-installBinary org.jbpm jbpm-human-task $VERSION_P04
-installBinary org.jbpm jbpm-test $VERSION_P04
+installUpdatedBinary org.jbpm jbpm-flow $VERSION_P05
+installUpdatedBinary org.jbpm jbpm-flow-builder $VERSION_P04
+installUpdatedBinary org.jbpm jbpm-persistence-jpa $VERSION_P02
+installUpdatedBinary org.jbpm jbpm-bam $VERSION_P02
+installUpdatedBinary org.jbpm jbpm-bpmn2 $VERSION_P05
+installUpdatedBinary org.jbpm jbpm-workitems $VERSION_P02
+installUpdatedBinary org.jbpm jbpm-human-task $VERSION_P05
+installUpdatedBinary org.jbpm jbpm-test $VERSION_P04
 
 cd ..
 rm -rf binaries
 rm jboss-brms-engine.zip
 rm jboss-jbpm-engine.zip
 
-echo Installation of binaries "for" BRMS $VERSION_P04 complete.
+echo Installation of binaries "for" BRMS $VERSION_P05 complete.
 echo
 
 
