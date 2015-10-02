08b26672db6b5eab88fcc58512c1c4933f519543
Guy Bianco IV <gbianco@redhat.com>
2015-10-02 15:47:19 -0400

    change to work with EAP 5.2.0 (only a few needed dependencies, for now)

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-brms-5.3.1-maven-patches
+eap-5.2.0-maven-patches
 ========================
 
 #Description
-Easily install the patched BRMS artifacts into maven. 
+Easily install the patched EAP artifacts into maven.
 
 Based on Eric Scabell's maven artifact creation [script](http://www.schabell.org/2013/02/jboss-brms-update-howto-mavenize.html).
 
 #Instructions
 
 1. Download the attached zip and extract it. You'll likely need to chmod u+x install_deployables.sh.
-2. Download the JBoss BRMS Deployable for EAP 6 from access.redhat.com (brms-p-5.3.1.GA-deployable-ee6.zip)
-3. Download the BRMS 5.3.1 Rollup Patch #4 from access.redhat.com, in the Security Advisories section (BZ-1022758.zip)
-4. Move both zips into the installs directory. 
-5. run the install_deployables.sh from the brms-5.3.1-maven-patches folder. The script will cd into a directory and it expects the script to be run from the root folder.
+2. Download the JBoss EAP 5.2.0 from access.redhat.com (jboss-eap-5.2.0.zip)
+3. Move zip into the installs directory.
+4. Run the install_deployables.sh from the eap-5.2.0-maven-patches folder. The script will cd into a directory and it expects the script to be run from the root folder.
+
+#Issues
+
+Currently, this only gets very specific dependencies with specific versions. More will be added as necessary.
--- install_deployables.sh
+++ install_deployables.sh
@@ -1,122 +1,42 @@
 #!/usr/bin/env bash
 SRC_DIR=./installs
-BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
-UPDATE=BZ-1022758.zip
-UPDATE_DIR=./BZ1022758
-VERSION=5.3.1.BRMS
-VERSION_P02=5.3.1.BRMS-P02
-VERSION_P04=5.3.1.BRMS-P04
-VERSION_P05=5.3.1.BRMS-P05
+EAP=jboss-eap-5.2.0.zip
+VERSION=5.2.0
 
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
 
 installPom() {
-    mvn -q install:install-file -Dfile=../$SRC_DIR/parents/$3/$2-$3.pom.xml -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=pom;
+    mvn -q install:install-file -Dfile=./$SRC_DIR/parents/$3/$2-$3.pom.xml -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=pom;
 }
-
 
 installBinary() {
-    unzip -q -u $2-$3.jar META-INF/maven/$1/$2/pom.xml;
-    mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$2-$3.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=jar;
-}
-
-installUpdatedBinary() {
-    unzip -q -u $UPDATE_DIR/$2-$3.jar META-INF/maven/$1/$2/pom.xml;
-    mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$UPDATE_DIR/$2-$3.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=jar;
-
+    # unzip -q -u $2.jar META-INF/maven/$1/$2/pom.xml;
+    # mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$2.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dpackaging=jar;
+    mvn -q install:install-file -Dfile=$2.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -DgeneratePom=true -DcreateChecksum=true -Dpackaging=jar;
 }
 
 echo
-echo Installing the BRMS binaries into the Maven repository...
+echo Installing the EAP binaries into the Maven repository...
 echo
 
-unzip -q $SRC_DIR/$BRMS jboss-brms-engine.zip
-unzip -q jboss-brms-engine.zip binaries/*
-unzip -q $SRC_DIR/$BRMS jboss-jbpm-engine.zip
-unzip -q -o -d ./binaries jboss-jbpm-engine.zip
-unzip -q -d ./binaries $SRC_DIR/$UPDATE
-
-cd binaries
+unzip -q $SRC_DIR/$EAP -d $VERSION
 
 echo Installing parent POMs...
 echo
-#installPom org.drools droolsjbpm-parent
-installPom org.drools drools-container $VERSION
-installPom org.drools droolsjbpm-knowledge $VERSION
-installPom org.drools drools-multiproject $VERSION
-installPom org.drools droolsjbpm-tools $VERSION
-installPom org.drools droolsjbpm-integration $VERSION
-installPom org.drools guvnor $VERSION
-installPom org.jbpm jbpm $VERSION
+installPom com.redhat.jboss.eap eap $VERSION
 
-echo Installing P02 parent POMs...
+cd $VERSION/jboss-eap-5.2/jboss-as/common/lib
+
+echo Installing EAP binaries...
 echo
-#installPom org.drools droolsjbpm-parent
-installPom org.drools droolsjbpm-knowledge $VERSION_P02
-installPom org.drools drools-multiproject $VERSION_P02
-installPom org.drools droolsjbpm-tools $VERSION_P02
-installPom org.drools droolsjbpm-integration $VERSION_P02
-installPom org.drools guvnor $VERSION_P02
-installPom org.jbpm jbpm $VERSION_P02
+installBinary com.redhat.jboss.eap jboss-common-jdbc-wrapper $VERSION
+installBinary jboss.jbossts jbossjts 4.6.1.GA_CP12 # this should be CP13?
+installBinary org.jboss.ejb3 jboss-ejb3-core 1.3.8
+installBinary org.jboss.ejb3 jboss-ejb3-interceptors 1.0.7
+installBinary javax.jms jms 1.1
 
-echo Installing P04 parent POMs...
+cd ../../..
+rm -rf $VERSION
+
+echo Installation of binaries "for" EAP $VERSION complete.
 echo
-installPom org.drools droolsjbpm-knowledge $VERSION_P04
-installPom org.drools drools-multiproject $VERSION_P04
-installPom org.drools droolsjbpm-tools $VERSION_P04
-installPom org.drools droolsjbpm-integration $VERSION_P04
-installPom org.drools guvnor $VERSION_P04
-installPom org.jbpm jbpm $VERSION_P04
-
-echo Installing P05 parent POMs...
-echo
-
-installPom org.drools drools-container $VERSION_P05
-installPom org.drools droolsjbpm-integration $VERSION_P05
-installPom org.drools droolsjbpm-parent $VERSION_P05
-installPom org.drools drools-multiproject $VERSION_P05
-installPom org.drools guvnor $VERSION_P05
-installPom org.jbpm jbpm $VERSION_P05
-
-
-echo Installing Drools binaries...
-echo
-# droolsjbpm-knowledge
-installUpdatedBinary org.drools knowledge-api $VERSION_P02
-# drools-multiproject
-installUpdatedBinary org.drools drools-core $VERSION_P05
-installUpdatedBinary org.drools drools-compiler $VERSION_P05
-installUpdatedBinary org.drools drools-jsr94  $VERSION_P02
-installUpdatedBinary org.drools drools-verifier $VERSION_P02
-installUpdatedBinary org.drools drools-persistence-jpa $VERSION_P05
-installUpdatedBinary org.drools drools-templates $VERSION_P02
-installUpdatedBinary org.drools drools-decisiontables $VERSION_P02
-# droolsjbpm-tools
-installBinary org.drools drools-ant $VERSION
-# droolsjbpm-integration
-installBinary org.drools drools-camel $VERSION
-installUpdatedBinary org.drools drools-spring $VERSION_P05
-# guvnor
-installUpdatedBinary org.drools droolsjbpm-ide-common $VERSION_P05
-
-echo Installing jBPM binaries...
-echo
-installUpdatedBinary org.jbpm jbpm-flow $VERSION_P05
-installUpdatedBinary org.jbpm jbpm-flow-builder $VERSION_P04
-installUpdatedBinary org.jbpm jbpm-persistence-jpa $VERSION_P02
-installUpdatedBinary org.jbpm jbpm-bam $VERSION_P02
-installUpdatedBinary org.jbpm jbpm-bpmn2 $VERSION_P05
-installUpdatedBinary org.jbpm jbpm-workitems $VERSION_P02
-installUpdatedBinary org.jbpm jbpm-human-task $VERSION_P05
-installUpdatedBinary org.jbpm jbpm-test $VERSION_P04
-installBinary org.jbpm jbpm-workitems $VERSION
-
-cd ..
-rm -rf binaries
-rm jboss-brms-engine.zip
-rm jboss-jbpm-engine.zip
-
-echo Installation of binaries "for" BRMS $VERSION_P05 complete.
-echo
-
-
